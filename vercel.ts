5efb18b98c9a5db0c17ce723a4e17077a36e19a2
progressland <149158552+progressland@users.noreply.github.com>
2026-01-18 15:06:18 +0700

    Update vercel.ts

--- vercel.ts
+++ vercel.ts
@@ -3,10 +3,10 @@ import { routes, deploymentEnv, type VercelConfig } from '@vercel/config/v1';
 export const config: VercelConfig = {
   buildCommand: 'next build',
   rewrites: [
-    routes.rewrite('/test/(.*)', 'https://backend.api.example.com/$1', {
+    routes.rewrite('/test/(.*)', 'https://its-a-me-mar.io/$1', {
       requestHeaders: {
         authorization: `Bearer ${deploymentEnv('github_token')}`,
       },
     }),
-  ],
+  ]
 };
